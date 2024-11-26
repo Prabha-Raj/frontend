4519b0391d2ae4d4578da5f1cf3fc80898006dcc
Prabha-Raj <116141429+Prabha-Raj@users.noreply.github.com>
2024-11-27 01:44:09 +0530

    from Label to lebel in Login.jsx

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,5 +1,5 @@
 import { Input } from '../ui/input';
-import { Label } from '../ui/Label';
+import { Label } from '../ui/label';
 import Navbar from '../shared/Navbar';
 import { RadioGroup } from '../ui/radio-group';
 import { Button } from '../ui/button';
